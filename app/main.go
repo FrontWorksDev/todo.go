07c3a3f95c5a863c9a48d0fb9425c5181fdc7578
FrontWorksDev <kengo.kojima@frontworks.dev>
2022-01-08 19:46:48 +0900

    feat: create middleware

--- app/main.go
+++ app/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"app/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	// middleware
+	engine.Use(middleware.RecordUpAndTime)
+
+	engine.Run()
+}
