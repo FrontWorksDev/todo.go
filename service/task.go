e0bbc67cffabb8220504677e416a6570081f7b5c
FrontWorksDev <kengo.kojima@frontworks.dev>
2022-01-23 23:20:40 +0900

    fixed: update mysql url

--- service/task.go
+++ service/task.go
@@ -14,7 +14,7 @@ type TaskService struct{}
 
 func (TaskService) SetTask(task *model.Task) error {
 	loadEnv()
-	DsName := os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@(" + os.Getenv("HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	DsName := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME")
 	err := errors.New("")
 	DbEngine, err = gorm.Open(mysql.Open(DsName), &gorm.Config{})
 	if err != nil && err.Error() != "" {
@@ -36,7 +36,7 @@ func (TaskService) SetTask(task *model.Task) error {
 
 func (TaskService) GetTaskList() []model.Task {
 	loadEnv()
-	DsName := os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@(" + os.Getenv("HOST") + ":3306)/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	DsName := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME")
 
 	err := errors.New("")
 	DbEngine, err = gorm.Open(mysql.Open(DsName), &gorm.Config{})
@@ -61,7 +61,7 @@ func (TaskService) GetTaskList() []model.Task {
 
 func (TaskService) UpdateTask(newTask *model.Task, id int) error {
 	loadEnv()
-	DsName := os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@(" + os.Getenv("HOST") + ":3306)/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	DsName := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME")
 
 	err := errors.New("")
 	DbEngine, err = gorm.Open(mysql.Open(DsName), &gorm.Config{})
@@ -85,7 +85,7 @@ func (TaskService) UpdateTask(newTask *model.Task, id int) error {
 
 func (TaskService) DeleteBook(id int) error {
 	loadEnv()
-	DsName := os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@(" + os.Getenv("HOST") + ":3306)/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	DsName := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME")
 
 	err := errors.New("")
 	DbEngine, err = gorm.Open(mysql.Open(DsName), &gorm.Config{})
